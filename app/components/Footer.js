1ba2846b5d431e90ac1677ec23d6e00c3d19a4d1
Derek Fidler <df@derekfidler.com>
2016-06-14 00:44:26 -0700

    results page styling

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,6 +5,8 @@ function Footer() {
   return (
     <footer className="site-footer" role="contentinfo">
       <ul>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Terms of Service</a></li>
         <li className="footer_text">© 2016 Shelter Tech, a 501(c)(3) nonprofit</li>
       </ul>
     </footer>
--- app/components/Header.js
+++ app/components/Header.js
@@ -6,11 +6,17 @@ let smallLogo = require('../assets/images/logo-small-white@3x.png');
 function Header() {
   return (
 	<header>
-    <nav className="navbar navbar-default" role="navigation">
+    <nav className="navbar" role="navigation">
       <a className="navbar-brand" href="#">
         <img src={smallLogo} alt="Ask Darcel"/>
       </a>
+      <div className="nav-search">
+        <div className="form-row" role="search">
+          <input type="text" className="search-field" placeholder="I'm looking for..." name="srch-term" id="srch-term" />
+        </div>
+      </div>
     </nav>
+    <div className="push"></div>
   </header>
   );
 }
