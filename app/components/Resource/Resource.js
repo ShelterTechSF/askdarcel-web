9c0641becce2e6766e56b8b1e62bc5b963ac6ac1
Richard Xia <richardxia@richardxia.com>
2017-12-19 09:11:14 -0800

    Revert "Fixed es-lint issue with line length"
    This reverts commit 2c20e12bcd19e169996805ae5a7d84bdaef38b66.

--- app/components/Resource/Resource.js
+++ app/components/Resource/Resource.js
@@ -70,7 +70,6 @@ class Resource extends Component {
 
             <header className="org--main--header">
               {
-                // eslint-disable-next-line no-use-before-define
                 <img className="certified" src={HAPcertified} alt="Verified by the Homeless Assistance Project" />
               }
               <h1 className="org--main--header--title">{resource.name}</h1>
