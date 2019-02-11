104683f75eaef0963d276daaa8a35b2972bedddd
Richard Xia <richardxia@richardxia.com>
2019-02-10 20:58:21 -0800

    Remove ListCategoryItems.

--- app/components/layout/CategoryList.jsx
+++ app/components/layout/CategoryList.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
 import CategoryItem from './CategoryItem';
-import ListCategoryItem from './ListCategoryItem';
 
 /* eslint-disable react/no-multi-comp */
 export class CategoryList extends React.Component {
   render() {
-    const categoryNodes = [];
-    const listCategoryNodes = [];
-
-    this.props.categories.forEach(category => {
-      if (category.featured) {
-        if (category.name === 'MOHCD Funded Services') {
-          categoryNodes.unshift(
-            <CategoryItem name={category.name} key={category.id} categoryid={category.id} />,
-          );
-        } else {
-          categoryNodes.push(
-            <CategoryItem name={category.name} key={category.id} categoryid={category.id} />,
-          );
-        }
-      } else {
-        listCategoryNodes.push(
-          <ListCategoryItem name={category.name} key={category.id} categoryid={category.id} />,
-        );
-      }
-    });
+    const categoryNodes = this.props.categories.map(category => (
+      <CategoryItem name={category.name} key={category.id} categoryid={category.id} />
+    ));
 
     return (
       <section className="category-list" role="main">
@@ -32,7 +14,6 @@ export class CategoryList extends React.Component {
           <span>Discover resources by category</span>
           <ul className="category-items"> {categoryNodes} </ul>
         </div>
-        <ul className="list-category-items"> {listCategoryNodes} </ul>
       </section>
     );
   }
