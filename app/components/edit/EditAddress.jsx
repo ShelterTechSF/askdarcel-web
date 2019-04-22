16c5dd686ad2d5273ccdd9d69f6967cf718f6804
quan.huynh <quan.huynh@salesforce.com>
2019-04-22 11:56:29 -0700

    add conditional rendering, fix label

--- app/components/edit/EditAddress.jsx
+++ app/components/edit/EditAddress.jsx
@@ -63,97 +63,88 @@ const hasNoLocation = (address) => {
 };
 
 const AddressForm = ({noLocation, handleNoLocationChange, handleAddressChange, address}) => {
-  if (noLocation) {
-    return (
-      <li key="address" className="edit--section--list--item">
-        <label htmlFor="address">Address</label>
+  return (
+    <li key="address" className="edit--section--list--item">
+      <label htmlFor="address">Address</label>
+      <label>
         <input
           type="checkbox"
           className="input-checkbox"
           checked={noLocation}
           onChange={handleNoLocationChange} 
         />
-        <div onClick={handleNoLocationChange}>No Physical Location</div>
-      </li>
-    );
-  } else {
-    return (
-      <li key="address" className="edit--section--list--item">
-        <label htmlFor="address">Address</label>
-        <input
-          type="checkbox"
-          className="input-checkbox"
-          checked={noLocation}
-          onChange={handleNoLocationChange} 
-        />
-        <div onClick={handleNoLocationChange}>No Physical Location</div>
-        <input
+        No Physical Location
+      </label>
+      {!noLocation &&
+        <div>
+          <input
           type="text"
           className="input"
           placeholder="Address 1"
           data-field="address_1"
           defaultValue={address.address_1}
           onChange={handleAddressChange}
-        />
-        <input
-          type="text"
-          className="input"
-          placeholder="Address 2"
-          data-field="address_2"
-          defaultValue={address.address_2}
-          onChange={handleAddressChange}
-        />
-        <input
-          type="text"
-          className="input"
-          placeholder="Address 3"
-          data-field="address_3"
-          defaultValue={address.address_3}
-          onChange={handleAddressChange}
-        />
-        <input
-          type="text"
-          className="input"
-          placeholder="Address 4"
-          data-field="address_4"
-          defaultValue={address.address_4}
-          onChange={handleAddressChange}
-        />
-        <input
-          type="text"
-          className="input"
-          placeholder="City"
-          data-field="city"
-          defaultValue={address.city}
-          onChange={handleAddressChange}
-        />
-        <input
-          type="text"
-          className="input"
-          placeholder="State/Province"
-          data-field="state_province"
-          defaultValue={address.state_province}
-          onChange={handleAddressChange}
-        />
-        <input
-          type="text"
-          className="input"
-          placeholder="Country"
-          data-field="country"
-          defaultValue={address.country}
-          onChange={handleAddressChange}
-        />
-        <input
-          type="text"
-          className="input"
-          placeholder="Postal/Zip Code"
-          data-field="postal_code"
-          defaultValue={address.postal_code}
-          onChange={handleAddressChange}
-        />
-      </li>
-    );
-  }
+          />
+          <input
+            type="text"
+            className="input"
+            placeholder="Address 2"
+            data-field="address_2"
+            defaultValue={address.address_2}
+            onChange={handleAddressChange}
+          />
+          <input
+            type="text"
+            className="input"
+            placeholder="Address 3"
+            data-field="address_3"
+            defaultValue={address.address_3}
+            onChange={handleAddressChange}
+          />
+          <input
+            type="text"
+            className="input"
+            placeholder="Address 4"
+            data-field="address_4"
+            defaultValue={address.address_4}
+            onChange={handleAddressChange}
+          />
+          <input
+            type="text"
+            className="input"
+            placeholder="City"
+            data-field="city"
+            defaultValue={address.city}
+            onChange={handleAddressChange}
+          />
+          <input
+            type="text"
+            className="input"
+            placeholder="State/Province"
+            data-field="state_province"
+            defaultValue={address.state_province}
+            onChange={handleAddressChange}
+          />
+          <input
+            type="text"
+            className="input"
+            placeholder="Country"
+            data-field="country"
+            defaultValue={address.country}
+            onChange={handleAddressChange}
+          />
+          <input
+            type="text"
+            className="input"
+            placeholder="Postal/Zip Code"
+            data-field="postal_code"
+            defaultValue={address.postal_code}
+            onChange={handleAddressChange}
+          />
+        </div>
+      }
+    </li>
+  );
 };
 
 export default EditAddress;
