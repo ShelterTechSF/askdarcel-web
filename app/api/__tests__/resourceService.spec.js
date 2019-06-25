11f99993ee623b3f7caa2a0974f3986310848f01
tsyaeger <tsyaeger@gmail.com>
2019-06-24 21:56:44 -0700

    removes done callback, adds working search resources req

--- app/api/__tests__/resourceService.spec.js
+++ app/api/__tests__/resourceService.spec.js
@@ -1,60 +1,65 @@
 import { expect } from 'chai';
 import MockAdapter from 'axios-mock-adapter';
 import axInstance from '../httpClient';
-import { resourceResponse } from './__mocks__/resourceMock';
-import {getResource, getResources, submitNewResource} from '../resourceService'
+import { resourceResponse } from './__mocks__/resourceResponseMock';
+import { resourceQueryResponse } from './__mocks__/resourceQueryResultsMock';
+import { newResource } from './__mocks__/newResourceMock';
+import {
+  getResource,
+  getResources,
+  searchForResources,
+  submitNewResource,
+} from '../resourceService';
 
 describe('Resources', () => {
-  let mockAdapter = new MockAdapter(axInstance);
+  const mockAdapter = new MockAdapter(axInstance);
 
-  it('Should return single resource', (done) => {
-     mockAdapter.onGet('/resources/5').reply(200, {
-       resourceResponse
-     });
-     const fetchResource = async (id) => await getResource(id)
-     let response = fetchResource(5)
-
-     setTimeout(() => {
-        expect(response.resources[0]).to.be.equal(resourceResponse);
-        expect(response.body).to.be.an('object');
-        expect(response.body.resources).to.be.an('array');
-    }, 50000)
-    done()
+  it('Should return single resource', async () => {
+    mockAdapter.onGet('/resources/5').reply(200, {
+      resourceResponse,
+    });
+    const response = await getResource(5);
+    const resources = response.data.resourceResponse.resources
+    expect(resources[0].name).to.be.equal('Test new');
+    expect(response).to.be.an('object');
+    expect(resources).to.be.an('array');
   });
 
-  it('Should return all resources', (done) => {
-     mockAdapter.onGet('/resources?category_id=all').reply(200, {
-       resourceResponse
-     });
-     const fetchResources = async () => await getResources()
-     let response = fetchResources()
-
-     setTimeout(() => {
-        expect(response.resources[0]).to.be.equal(resourceResponse);
-        expect(response.body).to.be.an('object');
-        expect(response.body.resources).to.be.an('array');
-    }, 50000)
-    done()
+  it('Should return all resources', async () => {
+    mockAdapter.onGet('/resources?category_id=all').reply(200, {
+      resourceResponse,
+    });
+    const response = await getResources();
+    expect(response).to.be.an('object');
+    expect(response.data.resourceResponse.resources).to.be.an('array');
   });
 
-  it('Should create a resource', (done) => {
-     mockAdapter.onPost('/resources',resourceResponse).reply(200, {
-       resourceResponse
-     });
-     const postResource = async () => await submitNewResource(resourceResponse)
-     let response = postResource()
-
-     setTimeout(() => {
-        expect(response.resources[0]).to.be.equal(resourceResponse);
-        expect(response.body).to.be.an('object');
-        expect(response.body.resources).to.be.an('array');
-    }, 50000)
-    done()
+  it('Should return resources that match search term', async () => {
+    const query = 'food';
+    mockAdapter.onGet(`/resources/search?query=${query}`).reply(200, {
+      resourceQueryResponse,
+    });
+    const response = await searchForResources(query);
+    const resources = response.data.resourceQueryResponse.resources
+    const checkQueryResults = resources.every((r) => {
+        return r.categories.some((c) => {
+            return c.name.toLowerCase() == query.toLowerCase()
+        })
+    })
+    expect(checkQueryResults).to.be.true
+    expect(response).to.be.an('object');
+    expect(resources).to.be.an('array');
   });
 
-// mock.onPost('/resources').reply(200);
-// mock.onPut('/product', { id: 4, name: 'foo' }).reply(204);
-// mock.onPut('/bar', { xyz: 'abc' }).reply(204)
 
-
+  it('Should create a resource', async () => {
+    mockAdapter.onPost('/resources', newResource).reply(200, {
+      resourceResponse,
+    });
+    // const response = await submitNewResource(newResource);
+  //
+  //    expect(response.data.resourceResponse.resources[0].name).to.be.equal('Test new');
+  //    expect(response).to.be.an('object');
+  //    expect(response.data.resourceResponse.resources).to.be.an('array');
+  });
 });
