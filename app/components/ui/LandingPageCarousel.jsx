e63c0251ec74c85aae6de060fe898a8b335416e5
drcaramelsyrup <dr.caramel.syrup@gmail.com>
2019-07-04 19:19:14 -0700

    Added a leftward nav button to the carousel.

--- app/components/ui/LandingPageCarousel.jsx
+++ app/components/ui/LandingPageCarousel.jsx
@@ -4,22 +4,39 @@ import { LandingPageCard, LandingPageTextCard } from './LandingPageCards';
 
 const CarouselWrapper = props => {
   return <div className="carousel-wrapper">{ props.children }</div>
-}
+};
 
 const CarouselContainer = props => {
   return <div className="carousel">{ props.children }</div>;
-}
+};
 
-class CarouselNavButton extends Component {
-  render() {
-    return (
-      <button className="carousel-nav" />
-    );
-  }
-}
+const CarouselNavButton = props => {
+  return <button className={`carousel-nav btn-${props.dir}`} />
+};
+CarouselNavButton.props = {
+  dir: PropTypes.string.isRequired,
+};
 
 class LandingPageCarousel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeIndex: 0
+    };
+  }
+
   render() {
+    const advanceRight = () => {
+      const incIndex = this.state.activeIndex + 1;
+      this.setState({ activeIndex: Math.min(incIndex, this.props.config.CARDS.length - this.props.config.NUM_SHOWN_CARDS) });
+    };
+
+    const advanceLeft = () => {
+      const decIndex = this.state.activeIndex - 1;
+      this.setState({ activeIndex: Math.max(0, decIndex) });
+    }
+
     return (
       <div>
       {
@@ -38,8 +55,12 @@ class LandingPageCarousel extends Component {
           }
           </CarouselContainer>
           {
-            this.props.config.CARDS.length > this.props.config.NUM_SHOWN_CARDS && 
-            <CarouselNavButton />
+            this.state.activeIndex > 0 &&
+              <CarouselNavButton dir="left" onClick={ advanceLeft } />
+          }
+          {
+            this.props.config.CARDS.length > this.props.config.NUM_SHOWN_CARDS + this.state.activeIndex && 
+              <CarouselNavButton dir="right" onClick={ advanceRight } />
           }
         </CarouselWrapper>
       }
--- app/components/ui/__tests__/LandingPageCarousel.spec.jsx
+++ app/components/ui/__tests__/LandingPageCarousel.spec.jsx
@@ -49,6 +49,21 @@ describe('<LandingPageCarousel />', () => {
       imgClass: 'legal-block-housing',
     },
   ];
+  const sevenCards = [
+    ...fiveCards,
+    {
+      title: 'Test6',
+      content: 'Test content 6',
+      query: 'Test+Query',
+      imgClass: 'legal-block-housing',
+    },
+    {
+      title: 'Test7',
+      content: 'Test content 7',
+      resource: 'https://www.google.com',
+      imgClass: 'legal-block-housing',
+    },
+  ];
   const validConfig = (cards, numShownCards) => {
     return {
       TITLE: {
@@ -65,15 +80,62 @@ describe('<LandingPageCarousel />', () => {
     expect(carousel.find('LandingPageTextCard')).to.have.lengthOf(twoCards.length);
   });
 
-  it('creates a navigation button when it has more cards than it can show at once', () => {
+  it('creates a rightward navigation button when it has more cards than it can show at once', () => {
     const carousel = shallow(<LandingPageCarousel config={validConfig(fiveCards, 4)} />);
     expect(carousel.find('LandingPageTextCard')).to.have.lengthOf(fiveCards.length);
     expect(carousel.find('CarouselNavButton')).to.have.lengthOf(1);
+    expect(carousel.find('CarouselNavButton').prop('dir')).to.equal('right');
   });
 
   it('should not have a navigation button when it can fit all its cards at once', () => {
     const carousel = shallow(<LandingPageCarousel config={validConfig(twoCards, 4)} />);
     expect(carousel.find('CarouselNavButton')).to.have.lengthOf(0);
+  });
+
+  const getNavBtn = (carousel, direction) => {
+    return carousel.findWhere(wrapper => wrapper.is('CarouselNavButton') && wrapper.prop('dir') === direction);
+  }
+
+  const getRightNavBtn = (carousel) => {
+    return getNavBtn(carousel, 'right');
+  };
+
+  it('advances to the next active index on clicking the right nav button', () => {
+    const carousel = shallow(<LandingPageCarousel config={validConfig(sevenCards, 4)} />);
+    expect(carousel.state('activeIndex')).to.equal(0);
+    getRightNavBtn(carousel).simulate('click');
+    expect(carousel.state('activeIndex')).to.equal(1);
+    getRightNavBtn(carousel).simulate('click');
+    expect(carousel.state('activeIndex')).to.equal(2);
+  });
+
+  it('does not show the right nav button after reaching the final navigable active index', () => {
+    const carousel = shallow(<LandingPageCarousel config={validConfig(fiveCards, 4)} />);
+    getRightNavBtn(carousel).simulate('click');
+    expect(getRightNavBtn(carousel)).to.have.lengthOf(0);
+  });
+
+  const getLeftNavBtn = (carousel) => {
+    return getNavBtn(carousel, 'left');
+  };
+
+  it('does not show a leftward nav button when the carousel is at the start', () => {
+    const carousel = shallow(<LandingPageCarousel config={validConfig(fiveCards, 4)} />);
+    expect(getLeftNavBtn(carousel)).to.have.lengthOf(0);
+  });
+
+  it('shows a leftward nav button when the index is not the starting one', () => {
+    const carousel = shallow(<LandingPageCarousel config={validConfig(fiveCards, 4)} />);
+    getRightNavBtn(carousel).simulate('click');
+    expect(getLeftNavBtn(carousel)).to.have.lengthOf(1);
+  });
+
+  it('goes to the previous active index when clicking on the left nav button', () => {
+    const carousel = shallow(<LandingPageCarousel config={validConfig(fiveCards, 4)} />);
+    const startIdx = carousel.state('activeIndex');
+    getRightNavBtn(carousel).simulate('click');
+    getLeftNavBtn(carousel).simulate('click');
+    expect(carousel.state('activeIndex')).to.equal(startIdx);
   });
 
 });
