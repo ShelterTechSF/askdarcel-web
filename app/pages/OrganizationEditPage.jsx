0e9ad7f32ac3793fd8f550604c149d073785ea08
mllsumulong <mllsumulong@gmail.com>
2019-09-08 16:19:57 -0700

    Fixed spacing issue

--- app/pages/OrganizationEditPage.jsx
+++ app/pages/OrganizationEditPage.jsx
@@ -784,7 +784,7 @@ class OrganizationEditPage extends React.Component {
             />
             <p>
 If you&#39;d like to add formatting to descriptions, we support
-              <a href="https://github.github.com/gfm/" target="_blank" rel="noopener noreferrer">Github flavored markdown</a>
+              <a href="https://github.github.com/gfm/" target="_blank" rel="noopener noreferrer"> Github Flavored Markdown</a>
 .
             </p>
           </li>
