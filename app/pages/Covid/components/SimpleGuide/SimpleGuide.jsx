f7228cf6f65123d40e1c4f61bf5e2a7d6ae25a41
Richard Xia <richardxia@richardxia.com>
2020-04-13 08:14:05 -0700

    Break up COVID pages into common components.

--- app/pages/Covid/components/SimpleGuide/SimpleGuide.jsx
+++ app/pages/Covid/components/SimpleGuide/SimpleGuide.jsx
@@ -1,38 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Iframe from 'react-iframe';
 
-import styles from './SimpleGuide.scss';
+import CovidHeader from '../CovidHeader';
+import GoogleDocEmbed from '../GoogleDocEmbed';
 
 
+/** A simple guide page that only consists of the common header and Google Doc
+ * embed.
+ */
 const SimpleGuide = ({ pageHeader, googleDocURL }) => (
-  <article className={styles.page}>
-    <header className={styles.header}>
-      <h1>
-        {pageHeader}
-      </h1>
-      <p>
-        This list is compiled to link individuals experiencing homelessness to resources.
-        We will be updating the list continuously, when services change.
-        <br />
-        Please email
-        {' '}
-        <a href="mailto:contact@sheltertech.org">contact@sheltertech.org</a>
-        {' '}
-        to suggest edits.
-      </p>
-    </header>
-    <section>
-      <Iframe
-        title={pageHeader}
-        url={googleDocURL}
-        width="80%"
-        height="100%"
-        padding="0pt"
-        allowFullScreen="yes"
-        className={styles.googleDocEmbed}
-      />
-    </section>
+  <article>
+    <CovidHeader headerText={pageHeader} />
+    <GoogleDocEmbed title={pageHeader} embedURL={googleDocURL} />
   </article>
 );
 
