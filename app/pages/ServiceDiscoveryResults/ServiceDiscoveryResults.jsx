71c33d9404d31780a79d4b1f9e37f644ea2eb1d4
Anna Kalkanis-Ellis <akalkanis@Annas-MacBook-Pro.local>
2020-06-11 00:00:12 -0700

    get algolia filtering working

--- app/pages/ServiceDiscoveryResults/ServiceDiscoveryResults.jsx
+++ app/pages/ServiceDiscoveryResults/ServiceDiscoveryResults.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { InstantSearch } from 'react-instantsearch/dom';
 import config from '../../config';
+import OpenNowFilter from './OpenNowFilter';
+import EligibilitiesListFilter from './EligibilitiesListFilter';
 
 import SearchResults from './SearchResults/SearchResults';
 import styles from './ServiceDiscoveryResults.scss';
@@ -26,6 +28,11 @@ export default class ServiceDiscoveryResults extends Component {
     this.handleClearAllClick = this.handleClearAllClick.bind(this);
     this.handleEligibilityClick = this.handleEligibilityClick.bind(this);
     this.handleOpenNowClick = this.handleOpenNowClick.bind(this);
+    this.onSearchStateChange = this.onSearchStateChange.bind(this);
+  }
+
+  onSearchStateChange(nextSearchState) {
+    this.setState({ searchState: nextSearchState });
   }
 
   handleClearAllClick() {
@@ -56,7 +63,6 @@ export default class ServiceDiscoveryResults extends Component {
     const { eligibilities } = this.props;
 
     const {
-      openNow,
       selectedEligibilities,
       searchState,
     } = this.state;
@@ -66,53 +72,57 @@ export default class ServiceDiscoveryResults extends Component {
         <div className={styles.header}>
           <h1 className={styles.title}>Food resources</h1>
         </div>
-        <div className={styles.flexContainer}>
-          <div className={styles.sidebar}>
-            <div className={styles.filterResourcesTitle}>Filter Resources</div>
-            <div
-              role="button"
-              tabIndex="0"
-              className={styles.clearAll}
-              onClick={this.handleClearAllClick}
-            >
+        <InstantSearch
+          appId={config.ALGOLIA_APPLICATION_ID}
+          apiKey={config.ALGOLIA_READ_ONLY_API_KEY}
+          indexName={`${config.ALGOLIA_INDEX_PREFIX}_services_search`}
+          searchState={searchState}
+          onSearchStateChange={this.onSearchStateChange}
+        >
+          <div className={styles.flexContainer}>
+            <div className={styles.sidebar}>
+              <div className={styles.filterResourcesTitle}>Filter Resources</div>
+              <div
+                role="button"
+                tabIndex="0"
+                className={styles.clearAll}
+                onClick={this.handleClearAllClick}
+              >
               Clear all
-            </div>
-            <div className={styles.filterGroup}>
-              <div className={styles.filterTitle}>Availability</div>
-              <label key="openNow" className={styles.checkBox}>
-                Open Now
-                <input type="checkbox" name="openNow" id="openNow" checked={openNow} onChange={this.handleOpenNowClick} />
-              </label>
-            </div>
+              </div>
+              <div className={styles.filterGroup}>
+                <div className={styles.filterTitle}>Availability</div>
+                <OpenNowFilter attribute="open_times" />
+              </div>
 
-            <div className={styles.filterGroup}>
-              <div className={styles.filterTitle}>Eligibilities</div>
-              {eligibilities.map(eligibility => (
-                <label key={eligibility.id} className={styles.checkBox}>
-                  {eligibility.name}
-                  <input
-                    type="checkbox"
-                    name={eligibility.name}
-                    id={eligibility.id}
-                    checked={selectedEligibilities[eligibility.id]}
-                    onChange={() => this.handleEligibilityClick(eligibility.id)}
-                  />
-                </label>
-              ))}
-            </div>
+              <div className={styles.filterGroup}>
+                <div className={styles.filterTitle}>Eligibilities</div>
+                <EligibilitiesListFilter
+                  attribute="eligibilities"
+                  limit={100}
+                  availableEligibilities={eligibilities}
+                  selectedEligibilities={selectedEligibilities}
+                />
+                {/* {eligibilities.map(eligibility => (
+                  <label key={eligibility.id} className={styles.checkBox}>
+                    {eligibility.name}
+                    <input
+                      type="checkbox"
+                      name={eligibility.name}
+                      id={eligibility.id}
+                      checked={selectedEligibilities[eligibility.id]}
+                      onChange={() => this.handleEligibilityClick(eligibility.id)}
+                    />
+                  </label>
+                ))} */}
+              </div>
 
-          </div>
-          <div className={styles.results}>
-            <InstantSearch
-              appId={config.ALGOLIA_APPLICATION_ID}
-              apiKey={config.ALGOLIA_READ_ONLY_API_KEY}
-              indexName={`${config.ALGOLIA_INDEX_PREFIX}_services_search`}
-              searchState={searchState}
-            >
+            </div>
+            <div className={styles.results}>
               <SearchResults />
-            </InstantSearch>
+            </div>
           </div>
-        </div>
+        </InstantSearch>
       </div>
     );
   }
