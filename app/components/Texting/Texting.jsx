e68d6543ad97aa1ca754d548184baeff7a0ddc10
fayceltouili <fayceltouili@gmail.com>
2020-11-19 14:17:50 -0800

    Catched errors and created error view

--- app/components/Texting/Texting.jsx
+++ app/components/Texting/Texting.jsx
@@ -8,12 +8,12 @@ import Loader from '../ui/Loader';
 
 // Initial state's values
 const initialState = {
-  name: '',
-  phoneNumber: '',
-  agreed: false,
+  name: 'Faycel Touili',
+  phoneNumber: '4134567788',
+  agreed: true,
   isLoading: false,
-  isSent: false,
-  errors: false,
+  isSent: true,
+  error: true,
 };
 
 // Text resaurce informations to the user phone
@@ -21,8 +21,7 @@ const initialState = {
 const Texting = ({ toggle, resource }) => {
 
   const [state, setState] = useState(initialState);
-  const { name, phoneNumber, agreed, isLoading, errors, isSent } = { ...state };
-
+  const { name, phoneNumber, agreed, isLoading, error, isSent } = { ...state };
   // Data to send to the Api
   // The logic in the values handles the missing infos
   const data = {
@@ -33,6 +32,7 @@ const Texting = ({ toggle, resource }) => {
     phoneHome: "",
     phoneWork: "",
     tags: resource.categories,
+    resourceId: resource.id,
     engagementType: "Resource Info",
     engagementInfo: {
         Org_Name: resource.name,
@@ -44,139 +44,143 @@ const Texting = ({ toggle, resource }) => {
         Org_Phone: resource.phones ? resource.phones[0].number : ''
     }
   };
-
-  // US phone number validator
+  
+  // Phone number validator: disable the user from clicking the submit button
+  // before providing a valid US phone number.
   const phoneNumberValidator = () => 
     /^\(?(\d{3})\)?[-\. ]?(\d{3})[-\. ]?(\d{4})$/.test(phoneNumber);
 
   const onSubmit = evt => {
     evt.preventDefault();
+    // Set isLoading to True to start the backend post request
     setState({ ...state, isLoading: true });
 
+    // Send data to backend
     const sendData = async () => {
       dataService.post(
-        "/api/textings", data ).then( response => {
-          response.ok ? 
-            setState({ ...state, isSent: true, isLoading: false })
-            : setState({ ...state, errors: true, isLoading: false });
-      });
+        "/api/textings", { data } ).then( response => {
+          if(response.ok) 
+            setState({ ...initialState, isSent: true })
+      }).catch( error =>
+        setState({ ...initialState, error })
+        );
     }
     sendData();
   };
 
   const onChange = evt => {
-    errors ? setState({ ...state, errors: false }) : null;
     const value = evt.target.type === 'checkbox' ? evt.target.checked : evt.target.value;
     const name = evt.target.name;
     setState({ ...state, [name]: value });
   };
 
-  // form view 
-  const form = () => {
+  // form view: show inputs 
+  const formView = () => {
     return (
-      ReactDOM.createPortal(
-        <div className="overlay">
-          <div className="popup">
-          {errors && alert("Something went wrong, try again!")}
-            <div className="closeIconDiv">
-              <img src={images.icon('close')} alt="close" className="closeIcon" onClick={toggle}/>
-            </div>
-            <div className="heading">
-              <h1 className="title">Text me information for <br></br><i>{resource.name}</i></h1>
-              <h3 className="description">You will receive their address and phone number.</h3>
-            </div>
-    
-            <div className="inputFields">
-              <div className="inputField">
-                <label className="label">First name (optional)</label>
-                <input
-                  type="text"
-                  name="name" 
-                  className="input" 
-                  value={name}
-                  data-field= "name"
-                  onChange={onChange}/>
-              </div>
-              
-              <div className="inputField">
-                <div className="label">Phone number (required)</div>
-                <div className="dataRates">*Standard text and data rates may apply.</div>
-                <input
-                  type="text"
-                  name="phoneNumber"
-                  className="input"
-                  value={phoneNumber}
-                  data-field="phoneNumber"
-                  onChange={onChange}/>
-              </div>
-    
-              <label className="checkBox dataRates" >
-                I agree to receive text messages from SF Service Guide.
-                <input
-                  type="checkbox"
-                  name="agreed"
-                  checked={agreed}
-                  data-field="agree"
-                  onChange={onChange}/>
-              </label>
-            </div>
-            <div className="buttons">
-              <button
-                type="button"
-                className="cancel button"
-                onClick={toggle}>
-                cancel
-              </button>
-              <button
-                type="submit"
-                className={`button${agreed && phoneNumberValidator(phoneNumber) ? '' : ' disabled'}`}
-                disabled={!(agreed && phoneNumberValidator(phoneNumber))}
-                data-field="submit"
-                onClick={onSubmit}>
-                Text me
-              </button>
-    
-            </div>
-            <div className="privacy">We will not share or sell your information with third party sites. 
-              For more information on privacy details, please go <a target="_blank" rel="noopener noreferrer" href="#">here</a> to view.
-            </div>
+      <>
+        <div className="heading">
+          <h1 className="title">Text me information for <br></br><i>{resource.name}</i></h1>
+          <h3 className="description">You will receive their address and phone number.</h3>
+        </div>
+
+        <div className="inputFields">
+          <div className="inputField">
+            <label className="label">First name (optional)</label>
+            <input
+              type="text"
+              name="name" 
+              className="input" 
+              value={name}
+              data-field= "name"
+              onChange={onChange}/>
           </div>
-        </div>, document.body
-      )
+          
+          <div className="inputField">
+            <div className="label">Phone number (required)</div>
+            <div className="dataRates">*Standard text and data rates may apply.</div>
+            <input
+              type="text"
+              name="phoneNumber"
+              className="input"
+              value={phoneNumber}
+              data-field="phoneNumber"
+              onChange={onChange}/>
+          </div>
+
+          <label className="checkBox dataRates" >
+            I agree to receive text messages from SF Service Guide.
+            <input
+              type="checkbox"
+              name="agreed"
+              checked={agreed}
+              data-field="agree"
+              onChange={onChange}/>
+          </label>
+        </div>
+
+        <div className="buttons">
+          <button
+            type="button"
+            className="cancel button"
+            onClick={toggle}>
+            cancel
+          </button>
+          <button
+            type="submit"
+            className={`button${agreed && phoneNumberValidator(phoneNumber) ? '' : ' disabled'}`}
+            disabled={!(agreed && phoneNumberValidator(phoneNumber))}
+            data-field="submit"
+            onClick={onSubmit}>
+            Text me
+          </button>
+        </div>
+
+        <div className="privacy">We will not share or sell your information with third party sites. 
+          For more information on privacy details, please go <a target="_blank" rel="noopener noreferrer" href="#">here</a> to view.
+        </div>
+      </>
     )
   };
 
-  // Loading spinner view 
-  const loading =  () => {
+  // Loading spinner: Show a loading spinner when the request being processed in the Backend 
+  const loadingView =  () => ( <Loader /> )
+
+  // Success sending view 
+  const sentView = () => {
     return (
-      <div className="overlay"> 
-        <div className="popup">
-          <Loader />
+      <div className="responseIcons">
+        <div className="circle">
+          <img src={images.icon('check-mark')} alt="check-mark" className="responseIcon"/>
+          <h1 className="responseText">Sent!</h1>
         </div>
       </div>
     )
   };
-
-  // Success sending view 
-  const sent = () => {
+  
+  // Error View: show Alert graphic and a friendly message
+  const errorView = () => {
     return (
+      <div className="responseIcons error">
+        <img src={images.icon('emergency')} alt="error" className="responseIcon"/>
+        <h1 className="responseText error">Sorry, something went wrong, please try again later.</h1>
+      </div>
+    )
+  };
+
+  // create a portal to render Texting component on Top of the DOM hierarchy
+  return (
+    ReactDOM.createPortal(
       <div className="overlay"> 
         <div className="popup">
           <div className="closeIconDiv">
             <img src={images.icon('close')} alt="close" className="closeIcon" onClick={toggle}/>
           </div>
-          <div className="sentIcons">
-            <div className="circle">
-              <img src={images.icon('check-mark')} alt="check-mark" className="checMarkkIcon" onClick={toggle}/>
-              <h1 className="sentText">Sent!</h1>
-            </div>
-          </div>
+          {error ? errorView() : isSent ? sentView() : isLoading ? loadingView() : formView()}
         </div>
-      </div>
+      </div>, document.body
     )
-  };
+  )
 
-  return isSent ? sent() : isLoading ? loading() : form();
 };
 
 Texting.propTypes = {
