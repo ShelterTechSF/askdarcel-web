ca17f6bd6ec050a7696c60a67378c857e330f47b
fayceltouili <fayceltouili@gmail.com>
2020-12-17 17:01:05 -0800

    :q

--- app/components/Texting/components/FormView/Heading.jsx
+++ app/components/Texting/components/FormView/Heading.jsx
@@ -5,9 +5,9 @@ import styles from './Form.module.scss';
 
 const Heading = ({ serviceName }) => (
   <div>
-    <div className={styles.title}>
-      Text me information for {serviceName}
-    </div>
+    <h1 className={styles.title}>
+      {`Text me information for ${serviceName}`}
+    </h1>
     <h3 className={styles.description}>
       You will receive their address and phone number.
     </h3>
