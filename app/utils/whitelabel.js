d15da50a763be82a0893e9c2ee7d29adea2dd17c
Leland Garofalo <lelandg@gmail.com>
2021-01-04 23:05:10 -0800

    Leland/white labebl (#1008)
    * Tweaking whitelabel select logic
    
    * white label tweaks

--- app/utils/whitelabel.js
+++ app/utils/whitelabel.js
@@ -2,7 +2,7 @@ import config from '../config';
 
 /** Whether we should display the SF Service Guide branded version of the site */
 const isSFFamiliesSite = () => window.location.host.indexOf(config.SFFAMILIES_DOMAIN) > -1;
-const isSFServiceGuideSite = () => !isSFFamiliesSite
+const isSFServiceGuideSite = () => !isSFFamiliesSite()
   && window.location.host.indexOf(config.MOHCD_DOMAIN) > -1;
 const getSiteTitle = () => {
   if (isSFServiceGuideSite()) {
