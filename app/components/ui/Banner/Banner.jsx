f78890f7965bd5ef25c437386eb8b776c6436847
Leland Garofalo <lgarofalo@users.noreply.github.com>
2021-01-17 18:39:26 -0800

    covid banner formatting fix

--- app/components/ui/Banner/Banner.jsx
+++ app/components/ui/Banner/Banner.jsx
@@ -6,7 +6,8 @@ export default function Banner() {
   return (
     <div className={styles.bannerContainer}>
       <strong>CORONAVIRUS COVID-19: </strong>
-      Get notified when it is your turn for the COVID-19 Vaccine and other relevant information <a href="https://sf.gov/covid-19-vaccine-san-francisco">HERE</a>
+      Get notified when it is your turn for the COVID-19 Vaccine and other relevant information&nbsp;
+      <a href="https://sf.gov/covid-19-vaccine-san-francisco">HERE</a>
     </div>
   );
 }
