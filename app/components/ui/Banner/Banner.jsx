1194cf52478ea39e7bfdaf3f92520713ba5f1d97
Leland Garofalo <lgarofalo@users.noreply.github.com>
2021-01-17 18:00:48 -0800

    Update covid banner text

--- app/components/ui/Banner/Banner.jsx
+++ app/components/ui/Banner/Banner.jsx
@@ -6,7 +6,7 @@ export default function Banner() {
   return (
     <div className={styles.bannerContainer}>
       <strong>CORONAVIRUS COVID-19: </strong>
-      Many organizations and services have reduced hours and availability. We are updating our data regularly.
+      Get notified when it is your turn for the COVID-19 Vaccine and other relevant information <a href="https://sf.gov/covid-19-vaccine-san-francisco">HERE</a>
     </div>
   );
 }
