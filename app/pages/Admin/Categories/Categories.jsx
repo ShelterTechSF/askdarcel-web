5348fab34c3d19776a38496caf4da24814e4fdfb
Ammar Alnatsheh <ammar.alnatsheh@gmail.com>
2021-02-04 11:15:34 -0800

    init Categories page & add AccordionMenu component

---
+++ app/pages/Admin/Categories/Categories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AccordionMenu from "./components/AccordionMenu";
+
+class Categories extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>initialize the Categories page</div>
+        <AccordionMenu />
+      </div>
+    );
+  }
+}
+
+export default Categories;
