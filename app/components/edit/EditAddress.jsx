5821214793183d86f535e04e627a0fccafe82b0f
Richard Xia <richardxia@richardxia.com>
2021-04-25 21:12:53 -0700

    Add EditAddressModal to EditAddress component.

--- app/components/edit/EditAddress.jsx
+++ app/components/edit/EditAddress.jsx
@@ -1,6 +1,118 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactModal from 'react-modal';
 import PropTypes from 'prop-types';
 import editCollectionHOC from './EditCollection';
+
+import s from './EditAddress.module.scss';
+
+// Subcomponents
+
+/** Modal containing the edit and add new address form.
+ *
+ * The form data is only synchronized with the outer component's state when the
+ * form is submitted. This allows the user to back out of any changes with the
+ * "Cancel" button. Therefore, we have chosen to implement the form inputs as
+ * uncontrolled components.
+ */
+const EditAddressModal = ({
+  isOpen, onRequestClose, defaultData, onSave,
+}) => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    onSave(Object.fromEntries(formData.entries()));
+    onRequestClose();
+  };
+  return (
+    <ReactModal
+      className={s.modal}
+      overlayClassName={s.overlay}
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+    >
+      <form className={s.modalContent} onSubmit={handleSubmit}>
+        <h1 className={s.title}>Add New Address</h1>
+        <div className={s.formBody}>
+          <label className={s.inputLabel}>
+            Address name (optional)
+            <input
+              className={s.inputTextbox}
+              type="text"
+              name="name"
+              placeholder="Name"
+              defaultValue={defaultData.name}
+            />
+          </label>
+          <div className={s.formControlGroup}>
+            <span className={s.formControlGroupLabel}>Street Address</span>
+            <input
+              className={s.inputTextbox}
+              type="text"
+              name="address_1"
+              placeholder="Address"
+              defaultValue={defaultData.address_1}
+            />
+            <input
+              className={s.inputTextbox}
+              type="text"
+              name="address_2"
+              placeholder="Apartment, suite, unit, building, floor, etc."
+              defaultValue={defaultData.address_2}
+            />
+          </div>
+          <label className={s.inputLabel}>
+            City
+            <input
+              className={s.inputTextbox}
+              type="text"
+              name="city"
+              placeholder="City"
+              defaultValue={defaultData.city}
+            />
+          </label>
+          <label className={s.inputLabel}>
+            State
+            <input
+              className={s.inputTextbox}
+              type="text"
+              name="state_province"
+              placeholder="State"
+              defaultValue={defaultData.state_province}
+            />
+          </label>
+          <label className={s.inputLabel}>
+            Zip code
+            <input
+              className={s.inputTextbox}
+              type="text"
+              name="postal_code"
+              placeholder="Zip Code"
+              defaultValue={defaultData.postal_code}
+            />
+          </label>
+        </div>
+        <div className={s.actionRow}>
+          <button
+            className={s.cancelButton}
+            type="button"
+            onClick={onRequestClose}
+          >
+            ← Cancel
+          </button>
+          <input
+            className={s.addButton}
+            type="submit"
+            value="Add new address"
+          />
+        </div>
+      </form>
+    </ReactModal>
+  );
+};
+
+EditAddressModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+};
 
 const HasPhysicalLocationToggle = ({ hasLocation, setHasLocation }) => (
   <label className="inline-checkbox">
@@ -13,7 +125,6 @@ const HasPhysicalLocationToggle = ({ hasLocation, setHasLocation }) => (
     No Physical Location
   </label>
 );
-
 
 const EditAddress = ({ index, item, handleChange }) => {
   const address = item;
@@ -110,16 +221,62 @@ EditAddress.propTypes = {
 const EditAddressCollection = editCollectionHOC(EditAddress, 'Addresses', {}, 'Add Address');
 EditAddressCollection.displayName = 'EditAddressCollection';
 
+// Main component
 
-const EditAddresses = ({ addresses, setHasLocation, setAddresses }) => (
-  <li key="address" className="edit--section--list--item">
-    <HasPhysicalLocationToggle
-      hasLocation={addresses.length !== 0}
-      setHasLocation={setHasLocation}
-    />
-    <EditAddressCollection collection={addresses} handleChange={setAddresses} />
-  </li>
-);
+// For when we actually add TypeScript support
+// type ModalState = { type: "closed" } | { type: "add" } | { type: "edit"; editingIndex: number };
+
+const EditAddresses = ({ addresses, setHasLocation, setAddresses }) => {
+  const [modalState, setModalState] = useState({ type: 'closed' });
+
+  const closeModal = () => setModalState({ type: 'closed' });
+
+  const modalIsOpen = modalState.type !== 'closed';
+  let modalDefaultData;
+  let modalOnSave;
+  switch (modalState.type) {
+    case 'closed':
+      modalDefaultData = {};
+      modalOnSave = () => {};
+      break;
+    case 'add': {
+      modalDefaultData = {};
+      modalOnSave = newData => setAddresses([...addresses, newData]);
+      break;
+    }
+    case 'edit': {
+      modalDefaultData = addresses[modalState.editingIndex];
+      modalOnSave = newData => {
+        const addressesCopy = addresses.slice();
+        addressesCopy[modalState.editingIndex] = newData;
+        setAddresses(addressesCopy);
+      };
+      break;
+    }
+    default:
+      throw new Error(`Unexpected modal state: ${modalState}`);
+  }
+
+  return (
+    <li key="address" className="edit--section--list--item">
+      <EditAddressModal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        defaultData={modalDefaultData}
+        onSave={modalOnSave}
+      />
+      <HasPhysicalLocationToggle
+        hasLocation={addresses.length !== 0}
+        setHasLocation={setHasLocation}
+      />
+      <EditAddressCollection collection={addresses} handleChange={setAddresses} />
+
+      <button type="button" onClick={() => setModalState({ type: 'add' })}>
+        Add location
+      </button>
+    </li>
+  );
+};
 
 EditAddresses.propTypes = {
   addresses: PropTypes.arrayOf(PropTypes.object).isRequired,
