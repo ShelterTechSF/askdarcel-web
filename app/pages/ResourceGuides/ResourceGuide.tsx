1fe1a4137236f7ac21bac5031c2a2eeb49df0524
lexholden <a@lexholden.com>
2021-04-26 20:38:54 -0400

    Cleanup and add 404 state

--- app/pages/ResourceGuides/ResourceGuide.tsx
+++ app/pages/ResourceGuides/ResourceGuide.tsx
@@ -1,35 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { ResourceGuideModel, ResourceGuideIFrame, ResourceGuidesData } from './layout';
+import { ResourceGuideIFrame, ResourceGuidesData } from './layout';
 import styles from './ResourceGuides.module.scss';
 import Footer from '../../components/ui/Footer/Footer';
 
 export const ResourceGuide = () => {
   const { id } = useParams<{ id: string }>();
-  const [guide, setGuide] = useState<ResourceGuideModel|null>(null);
   const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
+  const guide = useMemo(() => {
     const g = ResourceGuidesData.find(g => g.id === id);
-    if (g) {
-      setGuide(g);
-    }
-    setLoading(false);
-  }, [id]);
+    setLoading(false)
+    return g || null
+  }, [id])
 
   return (
     <>
       <div className={styles.resourceWrapper}>
-
         { loading && <p>Loading!</p> }
-
+        { !loading && !guide && <p>Cannot find a guide named {id}</p> }
         { guide && (
         <div className={styles.resourceFrameWrapper}>
           <h1>{ guide.name }</h1>
           <ResourceGuideIFrame title={guide.name} path={guide.path} />
         </div>
         ) }
-
       </div>
       <Footer />
     </>
