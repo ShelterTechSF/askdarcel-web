d5082f25a139ae8e132316f73bdecdf7fd0a2c5f
David Agustin <dagustin@myvest.com>
2021-07-11 23:03:35 -0700

    Fix branch error. Fix escape and codestyle in html.

--- app/components/ui/Banner/Banner.jsx
+++ app/components/ui/Banner/Banner.jsx
@@ -6,8 +6,14 @@ export default function Banner() {
   return (
     <div className={styles.bannerContainer}>
       <strong>COVID-19 VACCINE SITES: </strong>
-      Find out where to get a vaccine if you’re eligible&nbsp;
+      Find out where to get a vaccine if you&apos;re eligible&nbsp;
       <a href="https://sf.gov/vaccine-sites">HERE</a>
+      <br />
+      <strong>&nbsp;Need help applying for rent payment assistance?</strong>
+      &nbsp;Check out Code Tenderloin&apos;s workshops&nbsp;
+      <a href="https://docs.google.com/document/d/1t-RM30QQHA5UQdzIGHrD6kaFoBMrwmCBM7mEOBD8a9U/edit">HERE</a>
+      <br />
+      <a href="https://www.homeownershipsf.org/application-assistance-for-renters/">HERE</a>
     </div>
   );
 }
