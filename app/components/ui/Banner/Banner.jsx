75087ac77c88e1469a0e169d0b3a486a5a8d910d
Admin <admin@Admins-MBP.hitronhub.home>
2021-07-25 18:45:28 -0400

    Updates for banner link text

--- app/components/ui/Banner/Banner.jsx
+++ app/components/ui/Banner/Banner.jsx
@@ -6,15 +6,11 @@ export default function Banner() {
   return (
     <div className={styles.bannerContainer}>
       <strong>COVID-19 VACCINE SITES: </strong>
-      Find out where to get a vaccine if you&apos;re eligible&nbsp;
-      <a href="https://sf.gov/vaccine-sites">HERE</a>
+      Find out where to get a vaccine if you&apos;re eligible for a vaccine&#44; find a&nbsp;
+      <a target="_blank" href="https://sf.gov/vaccine-sites">site near you</a>.
       <br />
       <strong>&nbsp;Need help applying for rent payment assistance?</strong>
-      &nbsp;Check out Code Tenderloin&apos;s workshops&nbsp;
-      <a href="https://docs.google.com/document/d/1t-RM30QQHA5UQdzIGHrD6kaFoBMrwmCBM7mEOBD8a9U/edit">HERE</a>
-      <br />
-      and HomeownershipSF&apos;s resources&nbsp;
-      <a href="https://www.homeownershipsf.org/application-assistance-for-renters/">HERE</a>
+      &nbsp;Check out <a target="_blank" href="https://docs.google.com/document/d/1t-RM30QQHA5UQdzIGHrD6kaFoBMrwmCBM7mEOBD8a9U/edit">Code Tenderloin&apos;s workshops</a> and <a target="_blank" href="https://www.homeownershipsf.org/application-assistance-for-renters/">HomeownershipSF&apos;s resources</a>.&nbsp;
     </div>
   );
 }
