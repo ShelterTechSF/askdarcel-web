b0ab2af2d32051a0f44db1015e5f9668a0c4237d
lexholden <a@lexholden.com>
2021-08-10 13:18:53 -0400

    Fixing lintig

--- cypress/integration/service_page_spec.ts
+++ cypress/integration/service_page_spec.ts
@@ -1,14 +1,18 @@
 /// <reference types="cypress" />
 
 describe('Service Page', () => {
-  const serviceId = 1
+  const serviceId = 1;
 
   it('should load a basic page', async () => {
     cy.request(`/api/services/${serviceId}`).then(r => {
       cy.visit(`/services/${serviceId}`)
         .get('.listing--main header h1').first().should('have.text', r.body.service.name)
-        .get('.listing--main--left--about').first().should('exist')
-        .get('.listing--main--left--details').first().should('exist')
-    })
-  })
-})
+        .get('.listing--main--left--about')
+        .first()
+        .should('exist')
+        .get('.listing--main--left--details')
+        .first()
+        .should('exist');
+    });
+  });
+});
