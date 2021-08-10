c7892cbb41dffefeb04d82a8a2b55e79ab9c6356
lexholden <a@lexholden.com>
2021-08-10 14:46:47 -0400

    Adding check for response

--- cypress/integration/service_page_spec.ts
+++ cypress/integration/service_page_spec.ts
@@ -5,7 +5,8 @@ describe('Service Page', () => {
   const serviceId = 1;
 
   it('should load a basic page', async () => {
-    cy.request(`/api/services/${serviceId}`).then(r => {
+    cy.request(`/api/services/${serviceId}`).should(r => {
+      expect(r.isOkStatusCode).to.be.true('');
       cy.visit(`/services/${serviceId}`)
         .get('.listing--main header h1').first().should('have.text', r.body.service.name)
         .get('.listing--main--left--about').first().should('exist')
