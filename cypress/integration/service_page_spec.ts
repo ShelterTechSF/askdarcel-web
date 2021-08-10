73850a4bdd43c1ffaba04a9e64a50f16c9be0b34
lexholden <a@lexholden.com>
2021-08-10 15:51:10 -0400

    Stricter test

--- cypress/integration/service_page_spec.ts
+++ cypress/integration/service_page_spec.ts
@@ -6,9 +6,11 @@ describe('Service Page', () => {
 
   it('should load a basic page', async () => {
     cy.request(`/api/services/${serviceId}`).should(r => {
-      // expect(r.isOkStatusCode).to.be.true;
+      expect(r.status).to.eq(200);
+      expect(r.body.service).to.include.all.keys('id', 'name', 'long_description');
+
+      cy.viewport(1200, 1800);
       cy.visit(`/services/${serviceId}`)
-        .get('article.listing').screenshot()
         .get('.listing--main header h1').first().should('have.text', r.body.service.name)
         .get('.listing--main--left--about').first().should('exist')
         .get('.listing--main--left--details').first().should('exist');
