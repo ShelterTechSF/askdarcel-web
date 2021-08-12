a71331118d643e3db23095adfcb767ad2ea26ec7
Joe Freund <jfreund@myvest.com>
2021-08-11 19:57:39 -0700

    delint

--- app/components/ui/Banner/Banner.jsx
+++ app/components/ui/Banner/Banner.jsx
@@ -7,10 +7,15 @@ export default function Banner() {
     <div className={styles.bannerContainer}>
       <strong>COVID-19 VACCINE SITES: </strong>
       Find out where to get a vaccine if you&apos;re eligible for a vaccine&#44; find a&nbsp;
-      <a target="_blank" href="https://sf.gov/vaccine-sites">site near you</a>.
+      <a target="_blank" rel="noreferrer" href="https://sf.gov/vaccine-sites">site near you</a>
+      .
       <br />
       <strong>&nbsp;Need help applying for rent payment assistance?</strong>
-      &nbsp;Check out <a target="_blank" href="https://docs.google.com/document/d/1t-RM30QQHA5UQdzIGHrD6kaFoBMrwmCBM7mEOBD8a9U/edit">Code Tenderloin&apos;s workshops</a> and <a target="_blank" href="https://www.homeownershipsf.org/application-assistance-for-renters/">HomeownershipSF&apos;s resources</a>.&nbsp;
+      &nbsp;Check out&nbsp;
+      <a target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1t-RM30QQHA5UQdzIGHrD6kaFoBMrwmCBM7mEOBD8a9U/edit">Code Tenderloin&apos;s workshops</a>
+      &nbsp;and&nbsp;
+      <a target="_blank" rel="noreferrer" href="https://www.homeownershipsf.org/application-assistance-for-renters/">HomeownershipSF&apos;s resources</a>
+      .&nbsp;
     </div>
   );
 }
