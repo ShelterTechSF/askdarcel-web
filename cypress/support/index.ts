d27e6309dd0aad971782f44ba3879afb5651c669
David Agustin <dagustin@myvest.com>
2021-08-29 17:37:22 -0700

    Fix Cypress Error

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -19,6 +19,4 @@ import './commands';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-    return false
-})
+Cypress.on('uncaught:exception', () => false );
