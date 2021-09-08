2a1d672c60ffed90c0f09f27b63aa0df71339958
Richard Xia <richardxia@richardxia.com>
2021-09-08 00:29:02 -0700

    Convert RecurringSchedule to TypeScript. (#1072)
    * Convert RecurringSchedule to TypeScript.
    
    * Install ts-node and configure mocha to use it.
    
    * Move RecurringSchedule.ts to app/models.

--- app/components/listing/TableOfOpeningTimes.jsx
+++ app/components/listing/TableOfOpeningTimes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { RecurringSchedule } from '../../utils/RecurringSchedule';
+import { RecurringSchedule } from '../../models/RecurringSchedule';
 
 
 // TODO order with current day first
