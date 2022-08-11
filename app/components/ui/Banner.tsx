5c8314f976e8e7eaa767222d7e4026e7810fe3d2
Brian Schroer <schroer.brian@gmail.com>
2022-08-10 21:11:46 -0700

    PR improvements and mobile styles.

--- app/components/ui/Banner.tsx
+++ app/components/ui/Banner.tsx
@@ -6,12 +6,17 @@ export const Banner = () => (
   <div className={styles.bannerContainer}>
     <div className={styles.bannerContent}>
       <img src={icon('alert')} alt="attention" className={styles.alertIcon} />
-      <strong className={styles.title}>MONKEYPOX VACCINE INFORMATION: </strong>
-      Find out where to get a vaccine and if you&apos;re eligible.
-      Find a
-      {' '}
-      <a className={styles.bannerLink} target="_blank" rel="noreferrer" href="https://sf.gov/information/monkeypox-vaccine">site near you</a>
-      .
+      <div>
+        <strong className={styles.title}>MONKEYPOX VACCINE INFO: </strong>
+        Find out where to get a vaccine and if you&apos;re eligible.
+        {' '}
+        <span className={styles.linkInfo}>
+          Find a
+          {' '}
+          <a className={styles.bannerLink} target="_blank" rel="noreferrer" href="https://sf.gov/information/monkeypox-vaccine">site near you</a>
+          .
+        </span>
+      </div>
     </div>
   </div>
 );
