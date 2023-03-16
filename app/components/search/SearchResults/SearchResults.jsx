a23f28d59961e09483f926cacdd6d988202b9e95
Brian Schroer <schroer.brian@gmail.com>
2023-03-16 13:19:47 -0700

    Pass language param to pdf endpoint (#1233)
    * Pass language param to pdf endpoint
    
    * PR refactors
    
    * Small refactor
    
    ---------
    
    Co-authored-by: Brian Schroer <schroer.brianr@gmail.com>

--- app/components/search/SearchResults/SearchResults.jsx
+++ app/components/search/SearchResults/SearchResults.jsx
@@ -195,6 +195,7 @@ const SearchResult = ({ hit, index, setCenterCoords }) => {
     basePath = "services";
     entryId = serviceId;
   }
+  const handoutApiUrl = `/service-handout/${entryId}`;
 
   return (
     <div className={styles.searchResult}>
@@ -218,7 +219,37 @@ const SearchResult = ({ hit, index, setCenterCoords }) => {
                 {
                   key: -1,
                   description: "English",
-                  url: `/service-handout/${entryId}`,
+                  url: `${handoutApiUrl}`,
+                },
+                {
+                  key: -2,
+                  description: "Spanish",
+                  url: `${handoutApiUrl}?lang=es`,
+                },
+                {
+                  key: -3,
+                  description: "Tagalog",
+                  url: `${handoutApiUrl}?lang=tl`,
+                },
+                {
+                  key: -4,
+                  description: "Chinese (Traditional)",
+                  url: `${handoutApiUrl}?lang=zh-TW`,
+                },
+                {
+                  key: -5,
+                  description: "Vietnamese",
+                  url: `${handoutApiUrl}?lang=vi`,
+                },
+                {
+                  key: -6,
+                  description: "Russian",
+                  url: `${handoutApiUrl}?lang=ru`,
+                },
+                {
+                  key: -7,
+                  description: "Arabic",
+                  url: `${handoutApiUrl}?lang=ar`,
                 },
               ]
         }
