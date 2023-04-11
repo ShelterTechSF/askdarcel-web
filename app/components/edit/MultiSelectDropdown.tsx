0962cd15478732a1beddf2f0dfc3bd838118a0cb
Richard Xia <richardxia@richardxia.com>
2023-04-11 09:05:42 -0700

    Migrate Edit Page to TypeScript Part 6 (#1251)
    * Edit Page: Add types to InputField.
    
    * Edit Page: Add types to MultiSelectDropdown.
    
    This also adds @types/react-select, though we need to work around some
    inflexibility in the types of the version of the library that we're
    using related to multi-select vs single select mode.

--- app/components/edit/MultiSelectDropdown.tsx
+++ app/components/edit/MultiSelectDropdown.tsx
@@ -1,22 +1,67 @@
 import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
 import Select from "react-select";
 import * as dataService from "../../utils/DataService";
 import "react-select/dist/react-select.css";
 
-function dataToSelectValue(data) {
+/** Base type for a selectable option.
+ *
+ * This is the type that external consumers of this component see, and it is
+ * used as both the props for selected items as well as the handler for changing
+ * the set of selected items.
+ */
+interface SelectOption {
+  id: number;
+  name: string;
+}
+
+/** Internal type for interfacing with react-select.
+ *
+ * react-select requires that its inputs have a label field and a value field,
+ * so we internally wrap a SelectOption with this internal type.
+ */
+interface InternalSelectOption<T extends SelectOption> {
+  label: string;
+  value: T;
+}
+
+/** Transform a SelectOption to an InternalSelectOption. */
+function dataToSelectValue<T extends SelectOption>(
+  data: T
+): InternalSelectOption<T> {
   return {
     label: data.name,
     value: data,
   };
 }
 
-class MultiSelectDropdown extends Component<any, any> {
-  constructor(props) {
+type Props<T extends SelectOption> = {
+  selectedItems?: T[];
+  handleSelectChange: (newSelections: T[]) => void;
+  optionsRoute: string;
+  label: string;
+};
+
+type State<T extends SelectOption> = {
+  selectedValues: InternalSelectOption<T>[];
+  options: InternalSelectOption<T>[];
+};
+
+/** A multi-select dropdown menu.
+ *
+ * T is the type of the selection options, where T must conform to the
+ * SelectOption interface.
+ */
+class MultiSelectDropdown<T extends SelectOption> extends Component<
+  Props<T>,
+  State<T>
+> {
+  constructor(props: Props<T>) {
     super(props);
 
+    const { selectedItems = [] } = props;
+
     this.state = {
-      selectedValues: props.selectedItems.map(dataToSelectValue),
+      selectedValues: selectedItems.map(dataToSelectValue),
       options: [],
     };
 
@@ -32,7 +77,7 @@ class MultiSelectDropdown extends Component<any, any> {
     });
   }
 
-  handleChange(newValues) {
+  handleChange(newValues: InternalSelectOption<T>[]) {
     const { handleSelectChange } = this.props;
     this.setState({ selectedValues: newValues }, () => {
       handleSelectChange(newValues.map((val) => val.value));
@@ -50,30 +95,21 @@ class MultiSelectDropdown extends Component<any, any> {
           multi
           value={selectedValues}
           options={options}
-          onChange={this.handleChange}
+          /* This `as any` type assertion is necessary to work around a lack a
+           * flexibility in @types/react-select's types. When the Select
+           * component is configured in `multi` mode, onChange will send an
+           * array of values, not an individual value, but the current types
+           * require us to define a change handler that can accept either a
+           * single value or an array of values. We're currently using an
+           * ancient version of react-select, so it's possible that this has
+           * been fixed in future versions, especially since it looks like
+           * react-select v2 is a complete rewrite, and v5 comes with native
+           * TypeScript types. */
+          onChange={this.handleChange as any}
         />
       </>
     );
   }
 }
-
-// Leaving propTypes definitions here for reference. Remove when we add proper
-// TypeScript types to this component's props.
-//
-// MultiSelectDropdown.propTypes = {
-//   selectedItems: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.number.isRequired,
-//       name: PropTypes.string.isRequired,
-//     }).isRequired
-//   ),
-//   handleSelectChange: PropTypes.func.isRequired,
-//   optionsRoute: PropTypes.string.isRequired,
-//   label: PropTypes.string.isRequired,
-// };
-
-(MultiSelectDropdown as any).defaultProps = {
-  selectedItems: [],
-};
 
 export default MultiSelectDropdown;
--- app/components/edit/ProvidedService.tsx
+++ app/components/edit/ProvidedService.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import EditNotes from "./EditNotes";
 import EditSchedule from "./EditSchedule";
 import MultiSelectDropdown from "./MultiSelectDropdown";
@@ -108,7 +107,20 @@ const buildScheduleDays = (
 };
 export { buildScheduleDays };
 
-const InputField = ({ type, label, placeholder, value, setValue }) => (
+type InputFieldProps = {
+  type?: string;
+  label: string;
+  placeholder: string;
+  value?: string | null | undefined;
+  setValue: (x: string) => void;
+};
+const InputField = ({
+  type = "text",
+  label,
+  placeholder,
+  value = "",
+  setValue,
+}: InputFieldProps) => (
   <>
     <label htmlFor="input">{label}</label>
     <input
@@ -119,19 +131,6 @@ const InputField = ({ type, label, placeholder, value, setValue }) => (
     />
   </>
 );
-
-InputField.propTypes = {
-  type: PropTypes.string,
-  label: PropTypes.string.isRequired,
-  placeholder: PropTypes.string.isRequired,
-  value: PropTypes.string,
-  setValue: PropTypes.func.isRequired, // A function to call when setting a new value
-};
-
-InputField.defaultProps = {
-  type: "text",
-  value: "",
-};
 
 const EditAddresses = ({ service, resourceAddresses, handleChange }) => {
   const selectableOptions = resourceAddresses.flatMap((address, handle) => {
