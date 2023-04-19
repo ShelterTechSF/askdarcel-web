650ddb857084cd88f0ec8681075e7ef30f557ed0
Brian Schroer <schroer.brianr@gmail.com>
2023-04-19 16:43:55 -0700

    Prettier fixes

--- app/pages/EditBreakingNewsPage.tsx
+++ app/pages/EditBreakingNewsPage.tsx
@@ -1,44 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { RiAddBoxLine, RiDeleteBin5Line, RiSave3Line } from 'react-icons/ri';
+import React, { useState, useEffect } from "react";
+import { RiAddBoxLine, RiDeleteBin5Line, RiSave3Line } from "react-icons/ri";
 
-import * as dataService from '../utils/DataService';
-import { NewsArticle } from 'models';
-import './EditBreakingNewsPage.scss';
+import * as dataService from "../utils/DataService";
+import { NewsArticle } from "models";
+import "./EditBreakingNewsPage.scss";
 
 export const EditBreakingNewsPage = () => {
-  const [breakingNewsArticles, setBreakingNewsArticles] = useState<Array<NewsArticle>>([]);
+  const [breakingNewsArticles, setBreakingNewsArticles] = useState<
+    Array<NewsArticle>
+  >([]);
   useEffect(() => {
-    dataService.get('/api/news_articles').then(({ news_articles }) => {
-      setBreakingNewsArticles(news_articles.map((article: NewsArticle) => ({
-        ...article,
-        effective_date: formatDate(article.effective_date),
-        expiration_date: formatDate(article.expiration_date)
-      })));
-    })
+    dataService.get("/api/news_articles").then(({ news_articles }) => {
+      setBreakingNewsArticles(
+        news_articles.map((article: NewsArticle) => ({
+          ...article,
+          effective_date: formatDate(article.effective_date),
+          expiration_date: formatDate(article.expiration_date),
+        }))
+      );
+    });
   }, []);
 
   const formatDate = (date: string): string | null => {
-    return date ? new Date(date).toISOString().split('T')[0] : null;
-  }
+    return date ? new Date(date).toISOString().split("T")[0] : null;
+  };
 
   const onAddNew = async () => {
     setBreakingNewsArticles([
       ...breakingNewsArticles,
       {
-        id: '',
-        body: '',
-        effective_date: '',
-        expiration_date: '',
-        headline: '',
+        id: "",
+        body: "",
+        effective_date: "",
+        expiration_date: "",
+        headline: "",
         priority: null,
-        url: '',
-      }
+        url: "",
+      },
     ]);
-  }
+  };
 
   const onSave = (articleId: number) => {
-    const article = breakingNewsArticles.find(({ id }: any) => id === articleId);
-    const route = `/api/news_articles/${articleId ?? ''}`;
+    const article = breakingNewsArticles.find(
+      ({ id }: any) => id === articleId
+    );
+    const route = `/api/news_articles/${articleId ?? ""}`;
 
     if (articleId) {
       dataService.put(route, article);
@@ -48,7 +54,7 @@ export const EditBreakingNewsPage = () => {
   };
 
   const onDelete = async (articleId: number, index: number) => {
-    const updatedBreakingNewsArticles = [...breakingNewsArticles]
+    const updatedBreakingNewsArticles = [...breakingNewsArticles];
     updatedBreakingNewsArticles.splice(index, 1);
 
     if (articleId) {
@@ -63,16 +69,18 @@ export const EditBreakingNewsPage = () => {
     const { value, dataset } = target;
     const { field } = dataset;
 
-    setBreakingNewsArticles(breakingNewsArticles.map((article: any) => {
-      if (article.id === articleId) {
-        return {
-          ...article,
-          [field]: value
-        };
-      }
-      return article;
-    }));
-  }
+    setBreakingNewsArticles(
+      breakingNewsArticles.map((article: any) => {
+        if (article.id === articleId) {
+          return {
+            ...article,
+            [field]: value,
+          };
+        }
+        return article;
+      })
+    );
+  };
 
   const renderForm = (article: NewsArticle, index: number): any => (
     <form className="form" key={article.id}>
@@ -139,7 +147,10 @@ export const EditBreakingNewsPage = () => {
       </label>
       <label>
         URL (optional)
-        <p className="labelSubtext">Add an optional URL where the user can read more about the breaking news</p>
+        <p className="labelSubtext">
+          Add an optional URL where the user can read more about the breaking
+          news
+        </p>
         <input
           type="text"
           placeholder="https://www.readmore.com"
@@ -153,20 +164,18 @@ export const EditBreakingNewsPage = () => {
 
   return (
     <div className="edit-breaking-news-page">
-      <h1 className="breaking-news-header">
-        Edit Breaking News Articles
-      </h1>
+      <h1 className="breaking-news-header">Edit Breaking News Articles</h1>
       <div>
-        {breakingNewsArticles.map((article: any, index: number) => renderForm(article, index))}
+        {breakingNewsArticles.map((article: any, index: number) =>
+          renderForm(article, index)
+        )}
       </div>
       <div className="breaking-news-footer">
-        <button type="button"onClick={onAddNew}>
+        <button type="button" onClick={onAddNew}>
           <RiAddBoxLine />
           Add Breaking News Article
         </button>
       </div>
     </div>
   );
-}
-
-
+};
