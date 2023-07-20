3723e542bffe43c40f0e918bc282ed218befc2c6
Brian Schroer <schroer.brianr@gmail.com>
2023-07-20 16:34:06 -0700

    Migrate SearchResults.jsx to TypeScript

--- app/components/search/SearchResults/SearchResults.tsx
+++ app/components/search/SearchResults/SearchResults.tsx
@@ -8,38 +8,18 @@ import ResultsPagination from "components/search/Pagination/ResultsPagination";
 import { Texting } from "components/Texting";
 import { ClinicianActions } from "components/ucsf/ClinicianActions/ClinicianActions";
 import { ClientHandouts } from "components/ui/ClientHandoutsModal/ClientHandouts";
+import { SearchHit, transformHits } from "../../../models/SearchHits";
 import { icon } from "../../../assets";
-import { parseAlgoliaSchedule } from "../../../models";
 import styles from "./SearchResults.module.scss";
 
-/**
- * Transform Algolia search hits such that each hit has a recurringSchedule that
- * uses the time helper classes.
- */
-const transformHits = (hits) =>
-  hits.map((hit) => {
-    const inheritedSchedule =
-      hit.schedule && hit.schedule.length
-        ? hit.schedule
-        : hit.resource_schedule;
-    const recurringSchedule =
-      inheritedSchedule && inheritedSchedule.length
-        ? parseAlgoliaSchedule(inheritedSchedule)
-        : null;
-    return { ...hit, recurringSchedule };
-  });
 
-// Todo: setExpandList will be used as part of next stage of multiple location work
-// eslint-disable-next-line no-unused-vars
-const SearchResults = ({ searchResults, expandList, setExpandList }) => {
-  const [centerCoords, setCenterCoords] = useState(null);
+const SearchResults = ({ searchResults, expandList }) => {
+  const [centerCoords] = useState(null);
   const [googleMapObject, setMapObject] = useState(null);
 
   useEffect(() => {
-    if (centerCoords) {
-      googleMapObject.setCenter(centerCoords);
-    }
     document.body.classList.add("searchResultsPage");
+
     return () => {
       document.body.classList.remove("searchResultsPage");
     };
@@ -47,7 +27,7 @@ const SearchResults = ({ searchResults, expandList, setExpandList }) => {
 
   if (!searchResults) return null;
 
-  const hits = transformHits(searchResults.hits);
+  const hits = transformHits(searchResults.hits as SearchHit[]);
 
   return (
     <div className={styles.searchResultsAndMapContainer}>
@@ -64,14 +44,10 @@ const SearchResults = ({ searchResults, expandList, setExpandList }) => {
           No results found in your area. Try a different location, category, or
           search term.
         </div>
-        {hits.map((hit, index) => (
-          <SearchResult
-            hit={hit}
-            index={index}
-            setCenterCoords={setCenterCoords}
-            key={hit.id}
-          />
-        ))}
+        {hits.length &&
+          hits.map((hit, index) => (
+            <SearchResult hit={hit} index={index} key={hit.id} />
+          ))}
         <ResultsPagination noResults={!hits || !hits.length} />
       </div>
       <SearchMap
@@ -84,15 +60,15 @@ const SearchResults = ({ searchResults, expandList, setExpandList }) => {
   );
 };
 
-const SearchResult = ({ hit, index }) => {
+const SearchResult = ({ hit, index }: { hit: SearchHit; index: number }) => {
   const [textingIsOpen, setTextingIsOpen] = useState(false);
   const [clinicianActionsIsOpen, setClinicianActionsIsOpen] = useState(false);
   const [handoutModalIsOpen, setHandoutModalIsOpen] = useState(false);
 
   const listing = {
     listingName: hit.name,
-    serviceId: hit.type === "service" ? hit.id : null,
-    resourceId: hit.type === "resource" ? hit.id : null,
+    serviceId: hit.type === "service" ? hit.id : undefined,
+    resourceId: hit.type === "resource" ? hit.id : undefined,
   };
 
   const toggleTextingModal = () => setTextingIsOpen(!textingIsOpen);
@@ -153,7 +129,7 @@ const SearchResult = ({ hit, index }) => {
     </div>
   );
 
-  const renderAddressMetadata = (hit_) => {
+  const renderAddressMetadata = (hit_: SearchHit) => {
     if (!hit_.addresses || hit_.addresses.length === 0) {
       return <span>No address found</span>;
     }
@@ -166,7 +142,7 @@ const SearchResult = ({ hit, index }) => {
     return <span>No address found</span>;
   };
 
-  const phoneNumber = hit?.phones?.[0]?.number;
+  const phoneNumber = hit.type === "resource" ? hit.phones[0]?.number : null;
   const formatPhoneNumber = (number) => {
     // Takes 9 or 10 digit raw phone number input and outputs xxx-xxx-xxxx
     // If the input doesn't match regex, function returns number's original value
@@ -183,19 +159,11 @@ const SearchResult = ({ hit, index }) => {
     return number;
   };
 
-  const url = hit.url || hit.website;
-  const serviceId = hit.service_id;
-  const resourceId = hit.resource_id;
+  const url = hit.type === "service" ? hit.url : hit.website;
   const showDischargeSidelinks =
     whiteLabel.showClinicianAction && whiteLabel.showHandoutsIcon;
-  // Href structure varies depending on whether the hit is a resource or location
-  let basePath = "organizations";
-  let entryId = resourceId;
-  if (hit.type === "service") {
-    basePath = "services";
-    entryId = serviceId;
-  }
-  const handoutApiUrl = `/service-handout/${entryId}`;
+
+  const basePath = hit.type === "service" ? `services` : `organizations`;
 
   return (
     <div className={styles.searchResult}>
@@ -204,70 +172,79 @@ const SearchResult = ({ hit, index }) => {
         listing={listing}
         isShowing={textingIsOpen}
       />
-      <ClinicianActions
-        isOpen={clinicianActionsIsOpen}
-        setIsOpen={toggleClinicianActionsModal}
-        actions={hit?.instructions?.[0] ?? ""}
-      />
-      <ClientHandouts
-        isOpen={handoutModalIsOpen}
-        setIsOpen={toggleHandoutModal}
-        handoutCollection={[
-          {
-            key: -1,
-            description: "English",
-            url: `${handoutApiUrl}`,
-          },
-          {
-            key: -2,
-            description: "Spanish",
-            url: `${handoutApiUrl}?handoutLanguage=es`,
-          },
-          {
-            key: -3,
-            description: "Tagalog",
-            url: `${handoutApiUrl}?handoutLanguage=tl`,
-          },
-          {
-            key: -4,
-            description: "Chinese (Traditional)",
-            url: `${handoutApiUrl}?handoutLanguage=zh-TW`,
-          },
-          {
-            key: -5,
-            description: "Vietnamese",
-            url: `${handoutApiUrl}?handoutLanguage=vi`,
-          },
-          {
-            key: -6,
-            description: "Russian",
-            url: `${handoutApiUrl}?handoutLanguage=ru`,
-          },
-          {
-            key: -7,
-            description: "Arabic",
-            url: `${handoutApiUrl}?handoutLanguage=ar`,
-          },
-        ]}
-      />
+      {hit.type === "service" && (
+        <ClinicianActions
+          isOpen={clinicianActionsIsOpen}
+          setIsOpen={toggleClinicianActionsModal}
+          actions={hit.instructions?.[0] ?? ""}
+        />
+      )}
+      {hit.type === "service" && (
+        <ClientHandouts
+          isOpen={handoutModalIsOpen}
+          setIsOpen={toggleHandoutModal}
+          handoutCollection={[
+            {
+              key: -1,
+              description: "English",
+              url: `/service-handout/${hit.id}`,
+            },
+            {
+              key: -2,
+              description: "Spanish",
+              url: `/service-handout/${hit.id}?handoutLanguage=es`,
+            },
+            {
+              key: -3,
+              description: "Tagalog",
+              url: `/service-handout/${hit.id}?handoutLanguage=tl`,
+            },
+            {
+              key: -4,
+              description: "Chinese (Traditional)",
+              url: `/service-handout/${hit.id}?handoutLanguage=zh-TW`,
+            },
+            {
+              key: -5,
+              description: "Vietnamese",
+              url: `/service-handout/${hit.id}?handoutLanguage=vi`,
+            },
+            {
+              key: -6,
+              description: "Russian",
+              url: `/service-handout/${hit.id}?handoutLanguage=ru`,
+            },
+            {
+              key: -7,
+              description: "Arabic",
+              url: `/service-handout/${hit.id}?handoutLanguage=ar`,
+            },
+          ]}
+        />
+      )}
       <div className={styles.searchText}>
         <div className={styles.title}>
           <Link
-            to={{ pathname: `/${basePath}/${entryId}` }}
+            to={{ pathname: `/${basePath}/${hit.id}` }}
             className="notranslate"
           >{`${index + 1}. ${hit.name}`}</Link>
         </div>
-        <div className={styles.serviceOf}>
-          <Link to={`/organizations/${resourceId}`} className="notranslate">
-            {hit.service_of}
-          </Link>
-        </div>
+        {hit.type === "service" && (
+          <div className={styles.serviceOf}>
+            <Link
+              to={`/organizations/${hit.resource_id}`}
+              className="notranslate"
+            >
+              {hit.service_of}
+            </Link>
+          </div>
+        )}
         <div className={`notranslate ${styles.address}`}>
           {renderAddressMetadata(hit)}
         </div>
         <ReactMarkdown
           className={`rendered-markdown ${styles.description}`}
-          source={hit.long_description}
+          source={hit.long_description ?? undefined}
           linkTarget="_blank"
         />
       </div>
@@ -277,13 +254,15 @@ const SearchResult = ({ hit, index }) => {
             showDischargeSidelinks ? "" : styles.hideDischargeSidelinks
           }
         >
-          {!!hit.instructions?.length && clinicianActionsLink}
+          {hit.type === "service" &&
+            !!hit.instructions?.length &&
+            clinicianActionsLink}
           {handoutsLink}
         </div>
         <div
           className={showDischargeSidelinks ? styles.deemphasizeSideLinks : ""}
         >
-          {phoneNumber && (
+          (
             <div className={`${styles.sideLink} ${styles.showInPrintView}`}>
               <img
                 src={icon("phone-blue")}
@@ -295,7 +274,7 @@ const SearchResult = ({ hit, index }) => {
                 className={styles.sideLinkText}
               >{`Call ${formatPhoneNumber(phoneNumber)}`}</a>
             </div>
-          )}
+          )
           <div />
           {url && (
             <div className={styles.sideLink}>
