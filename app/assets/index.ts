2b55942b88e973fe244bd713ba73d911c9251de6
Richard Xia <richardxia@richardxia.com>
2023-09-17 12:05:44 -0700

    Migrate app/assets/index.js to TypeScript.

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -1,11 +1,11 @@
 const icons = require.context("../assets/img", true, /ic-.*\.(png|svg)$/i);
-const iconPathMap = {};
+const iconPathMap: Record<string, string> = {};
 
 icons.keys().forEach((key) => {
-  iconPathMap[key.match(/ic-([^@]*)(?:@3x)?.(?:svg|png)/)[1]] = icons(key);
+  iconPathMap[key.match(/ic-([^@]*)(?:@3x)?.(?:svg|png)/)![1]] = icons(key);
 });
 
-const icon = (name) =>
+const icon = (name: string): string =>
   iconPathMap[name.toLowerCase().replace(/(\s+|\/)/g, "-")];
 
 export { icon };
