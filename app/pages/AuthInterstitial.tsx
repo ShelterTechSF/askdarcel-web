0a7a889af4de1f5f3682fc81c43d5209d591adc9
Brian Schroer <schroer.brian@gmail.com>
2024-04-12 15:39:13 -0700

    1321 Add comments to interstitial page

--- app/pages/AuthInterstitial.tsx
+++ app/pages/AuthInterstitial.tsx
@@ -1,3 +1,11 @@
+/**
+ * This is the interstial redirect page after a user is authenticated via Auth0.
+ * The useEffect hook contains logic that checks the url for a hash, which contains an encoded
+ * access token. The code then intializes the user session using by decoding the token and
+ * passing the user props to the AppContext authState
+ *
+ */
+
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import * as AuthService from "utils/AuthService";
