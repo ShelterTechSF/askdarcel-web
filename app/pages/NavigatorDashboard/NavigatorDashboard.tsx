5ad7a2ffc9cc78b5cb0ca2a089b076a976bd548b
Brian Schroer <schroer.brian@gmail.com>
2024-04-23 18:19:31 -0700

    1324 Desktop and mobile styling of collapsed state of advanced search component

--- app/pages/NavigatorDashboard/NavigatorDashboard.tsx
+++ app/pages/NavigatorDashboard/NavigatorDashboard.tsx
@@ -1,19 +1,41 @@
 import React from "react";
+import { icon } from "assets";
+import { Button } from "components/ui/inline/Button/Button";
 import styles from "./NavigatorDashboard.module.scss";
+
+
+const AdvancedSearch = () => {
+  return (
+    <div className={styles.advancedSearchContainer}>
+      <h1 className={styles.header}>Hi, Navigator :)</h1>
+      <p className={styles.blurb}>
+        Please use the search bar to find resources for the individuals
+        you&apos;re assisting. You can enter keywords such as
+        &ldquo;shelter,&ldquo; &ldquo;food assistance,&ldquo; or
+        &ldquo;employment support,&ldquo; to discover relevant programs and
+        services.
+      </p>
+      <div className={styles.searchBoxContainer}>
+        <div className={styles.searchInputContainer}>
+          <input
+            type="text"
+            placeholder="Emergency Shelter"
+            className={styles.searchInput}
+          />
+        </div>
+        <Button addClass={styles.searchButton}>Search</Button>
+        <button type="button" className={styles.searchFilterButton}>
+          <img src={icon("filter-gray")} alt="Add filters to search" />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export const NavigatorDashboard = () => {
   return (
     <div>
-      <div className={styles.advancedSearchContainer}>
-        <h1 className={styles.greeting}>Hi, Case Managers :)</h1>
-        <p className={styles.blurb}>
-          Please use the search bar to find specific resources for the homeless
-          individuals you are assisting. You can enter keywords, such as
-          &ldquo;shelter,&ldquo; &ldquo;food assistance,&ldquo; or
-          &ldquo;employment support,&ldquo; to discover relevant programs and
-          services.
-        </p>
-      </div>
+      <AdvancedSearch />
     </div>
   );
 };
