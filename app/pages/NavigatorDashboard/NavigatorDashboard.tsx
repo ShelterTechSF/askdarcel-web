f6db93aedb298a8a0d2d3069fed1a94b6bf7f5f4
Brian Schroer <schroer.brian@gmail.com>
2024-05-07 14:06:41 -0700

    1324 Add category filters to Advanced Search component (#1345)
    * 1324 Desktop and mobile styling of collapsed state of advanced search component
    
    * 1324 Implement mobile styles and search functionality
    
    * Prettier
    
    * 1324 Add category filters to Advanced Search component
    
    * 1324 Add category refinements
    
    * 1324 Add eligibility filter dropdowns to Advanced Search
    
    * 1324 PR cleaenup and prettier
    
    * 1324 PR feedback
    
    * 1324 remove test static parent eligibility. oops

--- app/pages/NavigatorDashboard/NavigatorDashboard.tsx
+++ app/pages/NavigatorDashboard/NavigatorDashboard.tsx
@@ -1,20 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import qs from "qs";
+import * as Sentry from "@sentry/browser";
 import { useHistory } from "react-router-dom";
 
 import { icon } from "assets";
 import { Button } from "components/ui/inline/Button/Button";
+import { coreCategories } from "pages/HomePage";
+import { get } from "utils/DataService";
 
 import styles from "./NavigatorDashboard.module.scss";
+import { CategoryFilters } from "./CategoryFilters";
+import type { SelectedCategories } from "./CategoryFilters";
+import { EligibilityFilters } from "./EligibilityFilters";
+import type { SelectOptions } from "./EligibilityFilters";
+
+const initialSelectedCategories: SelectedCategories = {};
+coreCategories.forEach((category) => {
+  initialSelectedCategories[category.algoliaCategoryName] = false;
+});
+
+// TODO: Once we have created the parent eligibilities with children in our production DB,
+// this array will need to contain objects with their name and ID.
+const PARENT_ELIGIBILITIES: { name: string; id: number }[] = [];
+
+// Array containing arrays representing the selected child eligibilites of each parent eligibililty.
+const initialSelectedEligibilities = PARENT_ELIGIBILITIES.map(() => []);
 
 const AdvancedSearch = () => {
+  const [expandedOptions, setExpandedOptions] = useState(false);
+  const [selectedCategories, setSelectedCategories] = useState(
+    initialSelectedCategories
+  );
+  const [eligibilities, setEligibilities] = useState<SelectOptions[]>([]);
+  const [selectedEligibilities, setSelectedEligibilities] = useState<
+    SelectOptions[]
+  >(initialSelectedEligibilities);
+  const [searchValue, setSearchValue] = useState("");
+
   const history = useHistory();
-  let searchValue = "";
+
+  useEffect(() => {
+    // This makes a request to our API for all the eligibilities belonging to parent eligibility
+    // constants, which are defined above. It then maps the eligibilities to a list of select menu options.
+
+    // The eligibilities only need to be fetched one time as they will not change.
+    if (eligibilities.length) return;
+
+    const allPromises = PARENT_ELIGIBILITIES.map((eligibility) => {
+      return get(
+        `/api/eligibilities/subeligibilities?id=${eligibility.id}`
+      ).then((response) =>
+        response.eligibilities.map(
+          (childEligibility: { name: string; id: number }) => ({
+            label: childEligibility.name,
+            value: childEligibility.id,
+          })
+        )
+      );
+    });
+
+    Promise.all(allPromises)
+      .then((values) => {
+        setEligibilities(values);
+      })
+      .catch((err) => {
+        Sentry.captureException(err);
+      });
+  }, [eligibilities]);
+
   const submitSearch = () => {
+    if (!expandedOptions && !searchValue) return;
+
+    const searchState: { [key: string]: unknown } = {
+      refinementList: {
+        categories: coreCategories.flatMap((c) => {
+          if (selectedCategories[c.algoliaCategoryName]) {
+            return c.algoliaCategoryName;
+          }
+          return [];
+        }),
+        eligibilities: selectedEligibilities.flat().map((e) => {
+          return e.label;
+        }),
+      },
+    };
+
     if (searchValue) {
-      const query = qs.stringify({ query: searchValue });
-      history.push(`/search?${query}`);
+      searchState.query = searchValue;
     }
+
+    const search = qs.stringify(searchState, { encodeValuesOnly: true });
+    history.push(`/search?${search}`);
   };
 
   return (
@@ -39,17 +115,51 @@ const AdvancedSearch = () => {
             type="text"
             placeholder="Emergency Shelter"
             className={styles.searchInput}
-            onChange={(evt) => {
-              searchValue = evt.target.value;
-            }}
+            onChange={(evt) => setSearchValue(evt.target.value)}
           />
+          <Button
+            buttonType="submit"
+            // Rather using the disabled attr when advanced search mode is active, we pseudo-disable
+            // the button by adding styling. This is because using the disabled attr breaks using the
+            // "enter" button to submit the form when focused on the search input
+            addClass={`${styles.searchButton} ${
+              expandedOptions ? styles.disabled : ""
+            }`}
+          >
+            Search
+          </Button>
+          <button
+            type="button"
+            className={`${styles.searchFilterButton} ${
+              expandedOptions ? styles.expandedOptions : ""
+            }`}
+            onClick={() => setExpandedOptions(!expandedOptions)}
+          >
+            <img
+              src={icon(expandedOptions ? "filter-white" : "filter-gray")}
+              alt="Add filters to search"
+            />
+          </button>
         </div>
-        <Button buttonType="submit" addClass={styles.searchButton}>
-          Search
-        </Button>
-        <button type="button" className={styles.searchFilterButton}>
-          <img src={icon("filter-gray")} alt="Add filters to search" />
-        </button>
+        {expandedOptions && (
+          <>
+            <CategoryFilters
+              selectedCategories={selectedCategories}
+              setSelectedCategories={setSelectedCategories}
+            />
+            <EligibilityFilters
+              optionsGroupArray={eligibilities}
+              selectedEligibilities={selectedEligibilities}
+              setSelectedEligibilities={setSelectedEligibilities}
+              parentEligibilities={PARENT_ELIGIBILITIES}
+            />
+            <div className={styles.submitButtonRow}>
+              <Button buttonType="submit" addClass={styles.submitButton}>
+                Submit
+              </Button>
+            </div>
+          </>
+        )}
       </form>
     </div>
   );
