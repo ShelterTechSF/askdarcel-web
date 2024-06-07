152ae52719d75123040d36d62308cc79848367fc
Mark Buckner <buckner.a.mark@gmail.com>
2024-06-07 15:46:06 -0600

    update to use tokens; styling pass

--- app/pages/AboutPage2/AboutPage.tsx
+++ app/pages/AboutPage2/AboutPage.tsx
@@ -1,11 +1,33 @@
 import React, { useEffect, useState } from "react";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { client } from "../../sanity";
 import { Masthead } from "../../components/ui/Masthead/Masthead";
 import { EmailSignup } from "../../components/EmailSignup/Emailsignup";
 import { TwoColumnContentSection } from "../../components/ui/TwoColumnContentSection/TwoColumnContentSection";
 
+interface TwoColumnContentSection {
+  mediaAlignment: string;
+  image: SanityImageSource;
+  imageAlt: string | undefined;
+  contentBlock: any;
+  contentLinkButtonText: string;
+  contentLinkButtonUrl: string;
+  _id: string;
+}
+
+interface PageState {
+  pageInitialized: boolean;
+  mastHead: string;
+  twoColumnContentSections: TwoColumnContentSection[];
+}
+
 export const AboutPage = () => {
-  const [pageData, setPageData] = useState({ pageInitialized: false });
+  const [pageData, setPageData] = useState<PageState>({
+    pageInitialized: false,
+    mastHead: "",
+    twoColumnContentSections: [],
+  });
+
   useEffect(() => {
     const fetchPageData = async () => {
       const fetchedPageData = await client.fetch(
@@ -28,9 +50,11 @@ export const AboutPage = () => {
   return (
     <>
       <Masthead title={pageData.mastHead} />
-      {pageData?.twoColumnContentSections?.map((section: any) => {
-        return <TwoColumnContentSection {...section} />;
-      })}
+      {pageData?.twoColumnContentSections?.map(
+        (section: JSX.IntrinsicAttributes & TwoColumnContentSection) => {
+          return <TwoColumnContentSection key={section._id} {...section} />;
+        }
+      )}
       <EmailSignup />
     </>
   );
