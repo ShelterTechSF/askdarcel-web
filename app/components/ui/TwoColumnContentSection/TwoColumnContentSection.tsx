a97fcd0a8c123bd4c71882330e05f30455a2cfb9
Mark Buckner <buckner.a.mark@gmail.com>
2024-06-07 15:46:06 -0600

    update to use tokens; styling pass

--- app/components/ui/TwoColumnContentSection/TwoColumnContentSection.tsx
+++ app/components/ui/TwoColumnContentSection/TwoColumnContentSection.tsx
@@ -1,36 +1,54 @@
 import React from "react";
+import BlockContent from "@sanity/block-content-to-react";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import imageUrlBuilder from "@sanity/image-url";
 import styles from "./TwoColumnContentSection.module.scss";
 import { client } from "../../../sanity";
 
 const builder = imageUrlBuilder(client);
 
-const BlockContent = require("@sanity/block-content-to-react");
-
-export const TwoColumnContentSection = (props: any) => {
+export const TwoColumnContentSection = ({
+  image,
+  imageAlt,
+  contentBlock,
+  mediaAlignment,
+  contentLinkButtonText,
+  contentLinkButtonUrl,
+}: {
+  mediaAlignment: string;
+  image: SanityImageSource;
+  imageAlt: string | undefined;
+  contentBlock: any;
+  contentLinkButtonText: string;
+  contentLinkButtonUrl: string;
+}) => {
   return (
     <section className={styles.twoColumnContentSectionContainer}>
       <div
         className={
-          props.mediaAlignment === "left"
+          mediaAlignment === "left"
             ? styles.imageContainer_left
             : styles.imageContainer_right
         }
       >
         <img
           className={styles.image}
-          src={builder.image(props.image).url()}
-          alt={props.imageAlt}
+          src={builder.image(image).url()}
+          alt={imageAlt}
         />
       </div>
-      <BlockContent
-        blocks={props.contentBlock}
+      <div
         className={
-          props.mediaAlignment === "left"
+          mediaAlignment === "left"
             ? styles.contentContainer_left
             : styles.contentContainer_right
         }
-      />
+      >
+        <BlockContent className={styles.contentBlock} blocks={contentBlock} />
+        <a className={styles.contentLinkButton} href={contentLinkButtonUrl}>
+          {contentLinkButtonText}
+        </a>
+      </div>
     </section>
   );
 };
